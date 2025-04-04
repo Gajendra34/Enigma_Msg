fbb2d298168fe5d6c5426e423761eda0e345acc4
Gajendra34 <gajendra34940@gmail.com>
2025-04-04 14:32:26 +0530

    add hash password

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -45,14 +45,15 @@ export async function POST(request: Request) {
             } else {
                 // const salt=10;
                 const hashedPassword = await bcrypt.hash(password, salt);
+                // const hashedPassword = password;
                 existingUserByEmail.password = hashedPassword;
                 existingUserByEmail.verifyCode = verifyCode;
                 existingUserByEmail.verifyCodeExpiry = new Date(Date.now() + 3600000);  // 1hr=3600Sec or 3600000miliSec
                 await existingUserByEmail.save();
             }
         } else {
-            // const hashedPassword = await bcrypt.hash(password, salt);
-            const hashedPassword = password;
+            const hashedPassword = await bcrypt.hash(password, salt);
+            // const hashedPassword = password;
             const expiryDate = new Date();  //verify code expiry for 1 hour
             expiryDate.setHours(expiryDate.getHours() + 1);
 
