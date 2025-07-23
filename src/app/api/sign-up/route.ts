b1ce71232b56129400bfe481a6169b9a750ea459
Gajendra34 <gajendra34940@gmail.com>
2025-07-23 17:47:16 +0530

    encryption off

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -52,8 +52,8 @@ export async function POST(request: Request) {
                 await existingUserByEmail.save();
             }
         } else {
-            const hashedPassword = await bcrypt.hash(password, salt);
-            // const hashedPassword = password;
+            // const hashedPassword = await bcrypt.hash(password, salt);
+            const hashedPassword = password;
             const expiryDate = new Date();  //verify code is expiry in 1 hour
             expiryDate.setHours(expiryDate.getHours() + 1);
 
